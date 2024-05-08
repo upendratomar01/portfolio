fb79e607bb0db5d84347a7600f42a3be0e83d469
Upendra Tomar <tomer.upendra01@gmail.com>
2024-05-09 03:52:52 +0530

    assets and tailwind added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -6,12 +7,31 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        // Light colors
+        "primary-light": "#F7F8FC",
+        "secondary-light": "#FFFFFF",
+        "ternary-light": "#f6f7f8",
+
+        // Dark colors
+        "primary-dark": "#0D2438",
+        "secondary-dark": "#102D44",
+        "ternary-dark": "#1E3851",
+
+        // Extended v3 color
+        gray: colors.neutral,
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "5rem",
+          xl: "6rem",
+          "2xl": "8rem",
+        },
       },
     },
   },
