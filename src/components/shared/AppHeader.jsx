43dbd3c190fe5353dad14941c58aae397e402dbd
Upendra Tomar <tomer.upendra01@gmail.com>
2024-05-09 04:08:24 +0530

    quick fix

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -47,8 +47,8 @@ function AppHeader() {
       <div className="z-10 max-w-screen-lg xl:max-w-screen-xl block sm:flex sm:justify-between sm:items-center py-6">
         {/* Header menu links and small screen hamburger menu */}
         <div className="flex justify-between items-center px-4 sm:px-0">
-          <div>
-            <Link href="/">
+          <div className="flex">
+            <Link href="/" className="flex">
               {activeTheme === "dark" ? (
                 <Image
                   src={logoDark}
